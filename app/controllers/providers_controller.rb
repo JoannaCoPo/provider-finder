260e8cce5e819d971d850fec0e07ae86d3943e7b
JoannaCoPo <jococopo@gmail.com>
2021-12-06 15:27:28 -0700

    Remove unecessary conditional logic

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -5,13 +5,7 @@ class ProvidersController < ApplicationController
   end
 
   def index
-    if params[:taxonomy_description]
-      @providers = ProviderFacade.results_per_taxonomy_state(specialty_state_params)
-    elsif params[:first_name]
-      @providers = ProviderFacade.results_per_full_name(full_name_params)
-    else
-      array = ProviderFacade.results_per_name_state(params[:last_name], params[:state_abbreviation])
-      @providers = array.paginate(page: params[:page], per_page: 20)
-    end
+    array = ProviderFacade.all_queries(params[:first_name], params[:last_name], params[:taxonomy_desc], params[:state])
+    @providers = array.paginate(page: params[:page], per_page: 20)
   end
 end
