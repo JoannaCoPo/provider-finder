076264f12c0c1937f452abf56b0cb407d2b29d57
JoannaCoPo <jococopo@gmail.com>
2021-12-06 15:26:32 -0700

    Use one call with helper method and clean up facade

--- app/facades/provider_facade.rb
+++ app/facades/provider_facade.rb
@@ -1,27 +1,12 @@
 class ProviderFacade
   class << self
-    def results_per_name_state(last_name, state_abbreviation, limit = 200)
-      # decide where to incorpoate error handling
-      response = NpiService.last_name_state(last_name, state_abbreviation, limit)
+    def all_queries(first_name, last_name, taxonomy_desc, state, limit = 200)
+      response = NpiService.all_queries(first_name, last_name, taxonomy_desc, state, limit)
       create_provider_objects(response)
     end
 
-    def results_per_full_name(first_name, last_name)
-      full_name_data = NpiService.first_last_names(first_name, last_name)
-      # decide where to incorpoate error handling
-      # potentiallly use poros: FullName
-      FullName.new(full_name_data)
-    end
-
-    def results_per_taxonomy_state(taxonomy_description, state_abbreviation)
-      specialty_state_data = NpiService.first_last_names(taxonomy_description, state_abbreviation)
-      # decide where to incorpoate error handling
-      # potentiallly use poros: SpecialityState
-      SpecialityState.new(specialty_state_data)
-    end
-
     def create_provider_objects(response)
-      response[:results].map { |data| LastNameState.new(data) }
+      response[:results].map { |data| Provider.new(data) }
     end
   end
 end
