455efbba939768f0823197aa5fd7fb862a72eef0
JoannaCoPo <jococopo@gmail.com>
2021-12-07 11:29:24 -0700

    Remove comments and pry

--- app/services/npi_service.rb
+++ app/services/npi_service.rb
@@ -1,22 +1,7 @@
 class NpiService
   class << self
-    # def last_name_state(last_name, state, limit)
-    #   response = connection.get("/api/?version=2.0&last_name=#{last_name}&state=#{state}&limit=#{limit}")
-    #   JSON.parse(response.body, symbolize_names: true)
-    # end
-    #
-    # def first_last_names(first_name, last_name, limit)
-    #   response = connection.get("/api/?version=2.0&first_name=#{first_name}&last_name=#{last_name}&limit=#{limit}")
-    #   JSON.parse(response.body, symbolize_names: true)
-    # end
-    #
-    # def taxonomy_state(taxonomy_desc, state, limit)
-    #   response = connection.get("/api/?version=2.0&taxonomy_desc=#{taxonomy_desc}&state=#{state}&limit=#{limit}")
-    #   JSON.parse(response.body, symbolize_names: true)
-    # end
-
-    def all_queries(first_name, last_name,taxonomy_desc, state, limit)
-      response = connection.get("/api/?version=2.0&first_name=#{first_name}&last_name=#{last_name}&taxonomy_desc=#{taxonomy_desc}&state=#{state}&limit=#{limit}")
+    def all_queries(first_name, last_name, taxonomy_desc, state, limit)
+      response = connection.get("/api/?version=2.0&first_name=#{first_name}&last_name=#{last_name}&taxonomy_description=#{taxonomy_desc}&state=#{state}&limit=#{limit}")
       JSON.parse(response.body, symbolize_names: true)
     end
 
@@ -26,13 +11,3 @@ class NpiService
     end
   end
 end
-
-# defaults to 10 results, but also seems the max is 200?
-
-# The backend API calls serving the UI need to accept searches by:
-# ○ Lastname + 2 character state
-# ○ Firstname + Lastname
-# ○ Exact specialty + 2 character state
-
-#follow up and method naming conventions
-# note in readme decision to use verbose naming conventions
