cb83806feb81b81aea7cf719bab4672fdd306704
JoannaCoPo <jococopo@gmail.com>
2021-12-08 14:57:04 -0700

    Add root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root provider_search_path
+  
   get '/provider_search', to: 'providers#search'
   get '/provider_results', to: 'providers#index'
   get '/search_tips', to: 'providers#search_tips'
