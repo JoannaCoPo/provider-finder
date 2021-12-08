0f8e7edf00d287d0d299aaf8df691f3f6a06281b
JoannaCoPo <jococopo@gmail.com>
2021-12-07 19:14:46 -0700

    Change error handling of StandarError from rescue_from to 'rescue => e' per rails docs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
-  rescue_from StandardError, with: :add_search_criteria
-
-  def add_search_criteria
-    flash[:error] = message
-    redirect_back(fallback_location: provider_search_path)
-  end
-
-  def message
+  # rescue_from StandardError, with: :add_search_criteria
+  helper_method :flash_message
+  # def add_search_criteria
+  #   flash[:error] = message
+  #   redirect_back(fallback_location: provider_search_path)
+  # end
+  #
+  def flash_message
     'At least one the the following fields
     below must be completed in addition to
     State: First Name, Last Name, or specialty.'
