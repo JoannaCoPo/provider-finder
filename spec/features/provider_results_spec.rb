819ff15fabf73f8a72d9b87f01df1597a37d9997
JoannaCoPo <jococopo@gmail.com>
2021-12-07 19:12:57 -0700

    Test error handling

--- spec/features/provider_results_spec.rb
+++ spec/features/provider_results_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe "provider search results index page" do
-  describe "happy paths" do
+RSpec.describe 'provider search results index page' do
+  describe 'happy path' do
      before :each do
        visit provider_search_path
 
@@ -33,5 +33,37 @@ RSpec.describe "provider search results index page" do
      expect(page).to have_content('Provider Search Results:')
      expect(page).to have_content('Displaying 1 provider')
    end
+  end
+
+  describe 'sad path' do
+    it 'display a flash message if the only search input is state', :vcr do
+      flash_message = 'At least one the the following fields below'
+                       + 'must be completed in addition to State:'
+                       + 'First Name, Last Name, or specialty.'
+
+      visit provider_search_path
+
+      fill_in 'First name', with: ''
+      fill_in 'Last name', with: ''
+      fill_in 'Specialty or area of practice:', with: ''
+      fill_in 'Provider location by State', with: 'co'
+      click_on('Find me a provider!')
+
+      expect(page).to have_content(flash_message)
+      expect(current_path).to eq(provider_search_path)
+    end
+
+    it 'alerts user if no results were found via their input', :vcr do
+      visit provider_search_path
+
+      fill_in 'First name', with: 'don'
+      fill_in 'Last name', with: 'juan'
+      fill_in 'Specialty or area of practice:', with: ''
+      fill_in 'Provider location by State', with: ''
+      click_on('Find me a provider!')
+
+      expect(page).to have_content('No entries found')
+      expect(current_path).to eq(provider_results_path)
+    end
   end
 end
